0a22f6147a0c3c463745fea5192d96a835da7f3c
Oliwia <Oliweirdo@gmail.com>
2022-07-24 16:03:14 +0200

    revert changes

--- webpack.prod.js
+++ webpack.prod.js
@@ -28,29 +28,6 @@ module.exports = merge(common, {
       },
     ],
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 1,
-              modules: true,
-            },
-          },
-        ],
-        include: /\.module\.css$/,
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-        exclude: /\.module\.css$/,
-      },
-    ]
-  },
   plugins: [
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
